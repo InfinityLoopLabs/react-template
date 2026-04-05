6a428087721d2273547623d1e338b3bde685405c
ko <ko.kovalev@yadro.com>
2026-04-06 00:39:21 +0300

    refactor

--- app/application/store/store.ts
+++ app/application/store/store.ts
@@ -10,7 +10,7 @@ export const store = configureStore({
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredPaths: ['auth.keycloakInstance', 'roles.permissions'],
+        ignoredPaths: [],
         ignoredActions: [],
       },
     })

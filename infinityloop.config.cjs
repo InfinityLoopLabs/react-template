a92bc9d57c421355289827cf0dd1560ebd60c402
ko <ko.kovalev@yadro.com>
2026-04-06 00:26:07 +0300

    refactor

--- infinityloop.config.cjs
+++ infinityloop.config.cjs
@@ -1,22 +1,8 @@
 const TEMPLATE_REPO = "https://github.com/InfinityLoopLabs/react-template.git";
-const TARGET_REPO = "./";
 const TEMPLATE_REF = "main";
 
 module.exports = {
-  meta: {
-    templateRepo: TEMPLATE_REPO,
-    targetRepo: TARGET_REPO,
-    templateRef: TEMPLATE_REF,
-  },
   commands: {
-    bootstrap: [
-      {
-        type: "download",
-        repo: TEMPLATE_REPO,
-        ref: TEMPLATE_REF,
-        allowNonEmpty: true,
-      },
-    ],
     sync: [
       {
         type: "merge-template",

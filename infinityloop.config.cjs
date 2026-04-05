a88a25971e4b33aafc107bb35dd64ac7d9289c9d
ko <ko.kovalev@yadro.com>
2026-04-05 23:52:17 +0300

    refactor: migrate react template config to .cjs format

---
+++ infinityloop.config.cjs
@@ -0,0 +1,29 @@
+const TEMPLATE_REPO = "https://github.com/InfinityLoopLabs/react-template.git";
+const TARGET_REPO = "./";
+const TEMPLATE_REF = "main";
+
+module.exports = {
+  meta: {
+    templateRepo: TEMPLATE_REPO,
+    targetRepo: TARGET_REPO,
+    templateRef: TEMPLATE_REF,
+  },
+  commands: {
+    bootstrap: [
+      {
+        type: "download",
+        repo: TEMPLATE_REPO,
+        ref: TEMPLATE_REF,
+        allowNonEmpty: true,
+      },
+    ],
+    syncTemplate: [
+      {
+        type: "merge-template",
+        repo: TEMPLATE_REPO,
+        ref: TEMPLATE_REF,
+        allowUnrelatedHistories: true,
+      },
+    ],
+  },
+};
--- infinityloop.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  name: "React template config",
-};

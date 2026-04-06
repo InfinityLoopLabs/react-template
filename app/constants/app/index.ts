c052c611d691877ff6ce2941dfedea00d7c22ae5
ko <ko.kovalev@yadro.com>
2026-04-07 00:04:54 +0300

    add conditional execution, template value rendering, and `replace` plugin with tests

--- app/constants/app/index.ts
+++ app/constants/app/index.ts
@@ -7,9 +7,5 @@ export const IS_DEVELOPMENT_MODE = import.meta.env.DEV
 export const IS_PRODUCTION_MODE = import.meta.env.PROD
 export const BASE_URL = import.meta.env.BASE_URL
 
-export const MAIN_COLOR = '#ff6100'
-
-export const ARCANE_COLOR = '#ff6100'
-
 export const BACKEND_AUTH_URL = import.meta.env.VITE_BACKEND_AUTH_URL
 export const BACKEND_DRAFTS_URL = import.meta.env.VITE_BACKEND_DRAFTS_URL

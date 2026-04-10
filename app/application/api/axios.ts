0367f98897526d520267635adaa335b2d57dc78b
ko <ko.kovalev@yadro.com>
2026-04-10 20:29:15 +0300

    add conditional execution, template value rendering, and `replace` plugin with tests

--- app/application/api/axios.ts
+++ app/application/api/axios.ts
@@ -4,8 +4,6 @@ import { Mutex } from 'async-mutex'
 import axios from 'axios'
 import {
   // CLI: Paste backend urls
-  BACKEND_AUTH_URL,
-  BACKEND_DRAFTS_URL,
   LocalStorageEnum,
 } from '@constants/local'
 
@@ -20,44 +18,13 @@ export const headers = {
 // Для другого перехватчика, лучше создать отдельный экземпляр
 const mutex401 = new Mutex()
 
-export const auth_instance = axios.create({
-  baseURL: BACKEND_AUTH_URL,
-  headers,
-  // withCredentials: true,
-})
-
-export const drafts_instance = axios.create({
-  baseURL: BACKEND_DRAFTS_URL,
-  headers,
-  // withCredentials: true,
-})
-
 // CLI: Paste instances
 
 // Перехватчики запросов: Начало
-auth_instance.interceptors.request.use(config => {
-  config.headers!.Authorization = `Bearer ${getFromLocalStorage(LocalStorageEnum.ACCESS_TOKEN)}`
 
-  return config
-})
-
-drafts_instance.interceptors.request.use(config => {
-  config.headers!.Authorization = `Bearer ${getFromLocalStorage(LocalStorageEnum.ACCESS_TOKEN)}`
-
-  return config
-})
 // CLI: Paste request interceptors
 // Перехватчики запросов: Конец
 
 // Перехватчики ответов: Начало
-drafts_instance.interceptors.response.use(
-  config => config,
-  async error => await waitForTokenRefresh(error, mutex401, drafts_instance),
-)
-
-auth_instance.interceptors.response.use(
-  config => config,
-  async error => await waitForTokenRefresh(error, mutex401, auth_instance),
-)
 // CLI: Paste response interceptors
 // Перехватчики ответов: Конец
